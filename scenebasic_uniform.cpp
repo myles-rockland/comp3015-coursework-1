b6210445d9c42623db9c6c8d3837c8566122ff9d
Myles Rockland <myles.rockland@students.plymouth.ac.uk>
2025-02-26 21:09:03 +0000

    Add alpha discard

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -36,24 +36,50 @@ void SceneBasic_Uniform::initScene()
     view = glm::lookAt(vec3(1.0f, 1.25f, 1.25f), vec3(0.0f, 0.2f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
     projection = glm::mat4(1.0f);
 
-    // Set light uniforms for gun shader programs
-    gunProg.setUniform("Light.L", vec3(1.0f)); // Diffuse + Specular
-    gunProg.setUniform("Light.La", vec3(0.5f)); // Ambient
-
-    // Set light uniforms for plane shader programs
-    planeProg.use();
-    planeProg.setUniform("Light.L", vec3(1.0f)); // Diffuse + Specular
-    planeProg.setUniform("Light.La", vec3(0.5f)); // Ambient
-
-    // Set fog uniforms
+    // Set spotlights ambient diffuse specular uniforms
+    // Gun program
     gunProg.use();
-    gunProg.setUniform("Fog.MaxDist", 10.0f);
-    gunProg.setUniform("Fog.MinDist", 1.0f);
-    gunProg.setUniform("Fog.Colour", vec3(0.5f));
+    gunProg.setUniform("Spotlights[0].La", vec3(0.2f, 0.0f, 0.0f)); //0.2, 0, 0
+    gunProg.setUniform("Spotlights[0].L", vec3(0.8f, 0.0f, 0.0f)); //0.8, 0, 0
+    gunProg.setUniform("Spotlights[1].La", vec3(0.0f, 0.2f, 0.0f)); //0, 0.2, 0
+    gunProg.setUniform("Spotlights[1].L", vec3(0.0f, 0.8f, 0.0f)); //0, 0.8, 0
+    gunProg.setUniform("Spotlights[2].La", vec3(0.0f, 0.0f, 0.2f)); //0, 0, 0.2
+    gunProg.setUniform("Spotlights[2].L", vec3(0.0f, 0.0f, 0.8f)); //0, 0, 0.8
+    // Plane program
+    planeProg.use();
+    planeProg.setUniform("Spotlights[0].La", vec3(0.2f, 0.0f, 0.0f)); //0.2, 0, 0
+    planeProg.setUniform("Spotlights[0].L", vec3(0.8f, 0.0f, 0.0f)); //0.8, 0, 0
+    planeProg.setUniform("Spotlights[1].La", vec3(0.0f, 0.2f, 0.0f)); //0, 0.2, 0
+    planeProg.setUniform("Spotlights[1].L", vec3(0.0f, 0.8f, 0.0f)); //0, 0.8, 0
+    planeProg.setUniform("Spotlights[2].La", vec3(0.0f, 0.0f, 0.2f)); //0, 0, 0.2
+    planeProg.setUniform("Spotlights[2].L", vec3(0.0f, 0.0f, 0.8f)); //0, 0, 0.8
+
+    // Set spotlights exponent uniforms
+    for (int i = 0; i < 3; i++)
+    {
+        std::stringstream exponentName;
+        exponentName << "Spotlights[" << i << "].Exponent";
+        gunProg.use();
+        gunProg.setUniform(exponentName.str().c_str(), 50.0f);
+        planeProg.use();
+        planeProg.setUniform(exponentName.str().c_str(), 50.0f);
+    }
+
+    // Set spotlights cutoff uniforms
+    for (int i = 0; i < 3; i++)
+    {
+        std::stringstream cutoffName;
+        cutoffName << "Spotlights[" << i << "].Cutoff";
+        gunProg.use();
+        gunProg.setUniform(cutoffName.str().c_str(), radians(15.0f));
+        planeProg.use();
+        planeProg.setUniform(cutoffName.str().c_str(), radians(15.0f));
+    }
 
     // Load textures first
     GLuint diffuseTexture = Texture::loadTexture("media/38-special-revolver/textures/rev_d.tga.png");
     GLuint normalTexture = Texture::loadTexture("media/38-special-revolver/textures/rev_n.tga.png");
+    GLuint alphaTexture = Texture::loadTexture("media/texture/dots_alpha.png");
 
     // Load skybox texture
     GLuint skyboxTexture = Texture::loadHdrCubeMap("media/space_skybox/space");
@@ -63,6 +89,8 @@ void SceneBasic_Uniform::initScene()
     glBindTexture(GL_TEXTURE_2D, diffuseTexture);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, normalTexture);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, alphaTexture);
     
     // Set texture unit to 0 and bind cubemap
     glActiveTexture(GL_TEXTURE0);
@@ -105,6 +133,7 @@ void SceneBasic_Uniform::update( float t )
     }
     tPrev = t;
     
+    // Increase angle for rotation
     if (this->m_animate)
     {
         angle += rotSpeed * deltaT;
@@ -113,6 +142,8 @@ void SceneBasic_Uniform::update( float t )
             angle -= two_pi<float>();
         }
     }
+
+    // Get keyboard input
 }
 
 void SceneBasic_Uniform::render()
@@ -120,8 +151,33 @@ void SceneBasic_Uniform::render()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Set camera position and view matrix
-    vec3 cameraPos = vec3(15.0f * sin(angle), 3.0f, 15.0f * cos(angle));
+    //vec3 cameraPos = vec3(15.0f * sin(angle), 10.0f, 15.0f * cos(angle));
+    vec3 cameraPos = vec3(7.5f, 5.0f, 7.5f);
     view = glm::lookAt(cameraPos, vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+    mat3 normalMatrix = mat3(vec3(view[0]), vec3(view[1]), vec3(view[2]));
+
+    // Set spotlight uniforms
+    for (int i = 0; i < 3; i++)
+    {
+        // Position
+        std::stringstream positionName;
+        positionName << "Spotlights[" << i << "].Position";
+        float x = 50.0f * sin(angle + (two_pi<float>() / 3) * i); // 100
+        float z = 50.0f * cos(angle + (two_pi<float>() / 3) * i);
+        vec4 lightPos = vec4(x, 20.0f, z, 1.0f);
+        gunProg.use();
+        gunProg.setUniform(positionName.str().c_str(), view * lightPos);
+        planeProg.use();
+        planeProg.setUniform(positionName.str().c_str(), view * lightPos);
+
+        // Direction
+        std::stringstream directionName;
+        directionName << "Spotlights[" << i << "].Direction";
+        gunProg.use();
+        gunProg.setUniform(directionName.str().c_str(), normalMatrix * vec3(-lightPos.x, -lightPos.y, -lightPos.z)); // -lightPos.x, y and z
+        planeProg.use();
+        planeProg.setUniform(directionName.str().c_str(), normalMatrix * vec3(-lightPos.x, -lightPos.y, -lightPos.z)); // -lightPos.x, y and z
+    }
 
     // Skybox rendering
     skyboxProg.use();
@@ -133,9 +189,6 @@ void SceneBasic_Uniform::render()
 
     // Gun rendering
     gunProg.use();
-
-    // Set light uniforms
-    gunProg.setUniform("Light.Position", view * vec4(100.0f * cos(angle), 0.0f, 100.0f * sin(angle), 1.0f));
 
     // Set material uniforms
     gunProg.setUniform("Material.Kd", vec3(0.2f, 0.55f, 0.9f));
@@ -157,13 +210,10 @@ void SceneBasic_Uniform::render()
     // Plane rendering
     planeProg.use();
 
-    // Set light uniforms
-    planeProg.setUniform("Light.Position", view * vec4(100.0f * cos(angle), 0.0f, 100.0f * sin(angle), 1.0f));
-
     // Set plane material uniforms
     planeProg.setUniform("Material.Kd", vec3(0.5f));
     planeProg.setUniform("Material.Ks", vec3(0.0f));
-    planeProg.setUniform("Material.Ka", vec3(1.0f));
+    planeProg.setUniform("Material.Ka", vec3(0.5f));
     planeProg.setUniform("Material.Shininess", 1.0f);
 
     // Set plane model matrix
--- shader/basic_uniform.frag
+++ shader/basic_uniform.frag
@@ -9,6 +9,7 @@ layout (location = 0) out vec4 FragColor;
 
 layout (binding = 0) uniform sampler2D DiffuseTexture;
 layout (binding = 1) uniform sampler2D NormalTexture;
+layout (binding = 2) uniform sampler2D AlphaTexture;
 
 uniform struct LightInfo
 {
@@ -25,12 +26,15 @@ uniform struct MaterialInfo
     float Shininess;
 } Material;
 
-uniform struct FogInfo
+uniform struct SpotLightInfo
 {
-    float MaxDist;
-    float MinDist;
-    vec3 Colour;
-} Fog;
+    vec4 Position;
+    vec3 La;
+    vec3 L;
+    vec3 Direction;
+    float Exponent;
+    float Cutoff;
+} Spotlights[3];
 
 uniform mat4 ModelViewMatrix;
 uniform mat3 NormalMatrix;
@@ -61,13 +65,39 @@ vec3 blinnphong(vec3 n)
     return ambient + (diffuse + specular) * Light.L;
 }
 
-vec3 fog(vec3 n, vec3 bp)
+vec3 blinnphongSpot(vec3 n, int lightIndex)
 {
-    float dist = abs(Position.z);
-    float fogFactor = (Fog.MaxDist - dist)/(Fog.MaxDist - Fog.MinDist);
-    fogFactor = clamp(fogFactor, 0.0, 1.0);
-    vec3 colour = mix(Fog.Colour, bp, fogFactor);
-    return colour;
+    vec3 ambient = vec3(0), diffuse = vec3(0), specular = vec3(0);
+
+    // Get base texture colour from diffuse texture
+    vec3 texColour = texture(DiffuseTexture, TexCoord).rgb;
+
+    // Ambient
+    ambient = Spotlights[lightIndex].La * texColour;
+
+    // Diffuse
+    vec3 s = normalize(Spotlights[lightIndex].Position.xyz); //vec3 s = normalize(Spot.Position - position);
+
+    float cosAngle = dot(-s, normalize(Spotlights[lightIndex].Direction));
+    float angle = acos(cosAngle);
+    float spotScale;
+
+    if (angle >= 0.0f && angle < Spotlights[lightIndex].Cutoff)
+    {
+        spotScale = pow(cosAngle, Spotlights[lightIndex].Exponent);
+        float sDotN = max(dot(s, n), 0.0);
+        diffuse = texColour * sDotN;
+
+        // Specular
+        if (sDotN > 0.0)
+        {
+            vec3 v = normalize(ViewDir);
+            vec3 h = normalize(v + s);
+            specular = Material.Ks * pow(max(dot(h,n),0.0), Material.Shininess);
+        }
+    }
+
+    return ambient + spotScale * Spotlights[lightIndex].L * (diffuse + specular);
 }
 
 void main() {
@@ -75,11 +105,33 @@ void main() {
     vec3 norm = texture(NormalTexture, TexCoord).xyz;
     norm.xy = 2.0f * norm.xy - 1.0f; // - 1.0f applies to each component in the vector! https://learnwebgl.brown37.net/12_shader_language/glsl_mathematical_operations.html
 
-    // Calculate blinnphong
-    vec3 bp = blinnphong(normalize(norm));
+    vec3 Colour = vec3(0.0f, 0.0f, 0.0f);
 
-    // Calculate fog
-    vec3 colour = fog(norm, bp);
+    // Discard using alpha map
+    vec4 alphaMap = texture(AlphaTexture, TexCoord);
 
-    FragColor = vec4(colour, 1.0);
+    if (alphaMap.a < 0.15f)
+    {
+        discard;
+    }
+    else
+    {
+        if (gl_FrontFacing)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Colour += blinnphongSpot(normalize(norm), i);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Colour += blinnphongSpot(normalize(-norm), i);
+            }
+        }
+    }
+
+    // Apply Colour to FragColor
+    FragColor = vec4(Colour, 1.0);
 }
